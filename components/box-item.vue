fdde98dbf405f03d7d506547a95f78320efbbf6f
Fábio Mendes <fabiobends@gmail.com>
2023-04-26 10:08:34 -0300

    feat(box-item): add component
    This is a square box with a color which as height and width of 200px,
    but on the small version they are 100px.

---
+++ components/box-item.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="shadow rounded"
+    :class="[isSmall ? 'box-small' : 'box']"
+    :style="{
+      backgroundColor: '#' + hexColor,
+    }"
+  ></div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'BoxItem',
+  props: {
+    id: { type: String, default: '' },
+    isSmall: Boolean,
+    hexColor: { type: String, default: '' },
+    onClickBox: { type: Function, default: () => {} },
+  },
+}
+</script>
+
+<style>
+.box {
+  height: 200px;
+  width: 200px;
+}
+
+.box-small {
+  height: 100px;
+  width: 100px;
+}
+</style>
