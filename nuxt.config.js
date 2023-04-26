e5b85df6cbe44ab95ebd05cc8a9aa18812388f2b
Fábio Mendes <fabiobends@gmail.com>
2023-04-26 10:02:03 -0300

    feat(BootstrapVue): enable icons to the project

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,6 +43,10 @@ export default {
     'cookie-universal-nuxt',
   ],
 
+  bootstrapVue: {
+    icons: true,
+  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
